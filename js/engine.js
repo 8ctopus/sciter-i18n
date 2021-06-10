aaa9b311f9b3b5a1155625698ba6c4a04c199f9c
8ctopus <hello@octopuslabs.io>
2021-06-10 12:00:31 +0500

    added: more translations fixed: one type of checkbox skinning

--- js/engine.js
+++ js/engine.js
@@ -12,29 +12,68 @@ export class Engine
     {
         const fr = {
             translation: {
-                "description": "Test de traduction basée sur i18next",
-                "hello": "bonjour",
-                "howareyou": "Comment allez-vous aujourd'hui?",
-                "button": "changer la langue",
-                "Widget update successful.": "Succès de la mise-à-jour de widget.",
-                //"Widget failed with error 18.": "Widget a généré une erreur 18.",
-                "Widget could not be updated.": "Widget n'a pas pu être mis-à-jour.",
-                "Do you want to update widget?": "Voulez-vous mettre à jour widget?",
+                // first tab
+                "i18n engine test": "test du moteur i18n",
+                "h1": "h1 (fr)",
+                "h2": "h2 (fr)",
+                "h3": "h3 (fr)",
+                "p": "p (fr)",
+                "span": "span (fr)",
+                "label": "label (fr)",
+                "list item": "objet de la liste",
+
+                // buttons
                 "Default": "Défaut",
                 "Do it!": "Fais-le!",
                 "Disabled": "Désactivé",
                 "Success": "Succès",
                 "Warning": "Attention",
                 "Danger": "Danger",
+
+                // editboxes
                 "input text here": "entrer le texte ici",
                 "password": "mot de passe",
-                " list item ": "objet de la liste",
+                "nullable": "peut être nul",
+                "disabled": "désactivé",
+
+                // numbers
+                "number": "nombre",
+
+                // radiobuttons
+                "Yes": "Oui",
+                "No": "Non",
+                "Undefined": "Non-défini",
+                "Disabled (Checked)": "Désactivé (Coché)",
+
+                // checkboxes
+                "Checked": "Coché",
+                "Unchecked": "Non-coché",
+                "Mixed": "Mixte",
+                "checked state": "état coché",
+                "unchecked state": "état non-coché",
+
+                // toggle
+                "no label": "sans étiquette",
+                "group": "groupe",
+
+                // switch
+                "Left": "Gauche",
+                "Center": "Centre",
+                "Right": "Droite",
+
+                // dialog buttons
                 "info": "info",
                 "error": "erreur",
                 "warning": "avertissement",
                 "question": "question",
                 "select file": "sélectionner le fichier",
                 "select folder": "sélectionner le dossier",
+
+                // dialogs
+                "Widget update successful.": "Succès de la mise-à-jour de widget.",
+                //"Widget failed with error 18.": "Widget a généré une erreur 18.",
+                "Widget could not be updated.": "Widget n'a pas pu être mis-à-jour.",
+                "Do you want to update widget?": "Voulez-vous mettre à jour widget?",
             }
         };
 
