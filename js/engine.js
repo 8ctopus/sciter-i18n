88cdb2fb5a6580c2bda5d2a2e0a94c1aa6648571
8ctopus <hello@octopuslabs.io>
2021-06-10 14:03:45 +0500

    improved: load translation from file

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,6 @@
 import i18next from "i18next-20.3.1/src/index.js";
+import * as sys from "@sys";
+import {encode,decode} from "@sciter";
 
 export class Engine
 {
@@ -10,72 +12,10 @@ export class Engine
      */
     static init()
     {
-        const fr = {
-            translation: {
-                // first tab
-                "i18n engine test": "test du moteur i18n",
-                "h1": "h1 (fr)",
-                "h2": "h2 (fr)",
-                "h3": "h3 (fr)",
-                "p": "p (fr)",
-                "span": "span (fr)",
-                "label": "label (fr)",
-                "list item": "objet de la liste",
+        let buffer = sys.fs.$readfile("locales/fr.json");
+        buffer = decode(buffer);
 
-                // buttons
-                "Default": "Défaut",
-                "Do it!": "Fais-le!",
-                "Disabled": "Désactivé",
-                "Success": "Succès",
-                "Warning": "Attention",
-                "Danger": "Danger",
-
-                // editboxes
-                "input text here": "entrer le texte ici",
-                "password": "mot de passe",
-                "nullable": "peut être nul",
-                "disabled": "désactivé",
-
-                // numbers
-                "number": "nombre",
-
-                // radiobuttons
-                "Yes": "Oui",
-                "No": "Non",
-                "Undefined": "Non-défini",
-                "Disabled (Checked)": "Désactivé (Coché)",
-
-                // checkboxes
-                "Checked": "Coché",
-                "Unchecked": "Non-coché",
-                "Mixed": "Mixte",
-                "checked state": "état coché",
-                "unchecked state": "état non-coché",
-
-                // toggle
-                "no label": "sans étiquette",
-                "group": "groupe",
-
-                // switch
-                "Left": "Gauche",
-                "Center": "Centre",
-                "Right": "Droite",
-
-                // dialog buttons
-                "info": "info",
-                "error": "erreur",
-                "warning": "avertissement",
-                "question": "question",
-                "select file": "sélectionner le fichier",
-                "select folder": "sélectionner le dossier",
-
-                // dialogs
-                "Widget update successful.": "Succès de la mise-à-jour de widget.",
-                //"Widget failed with error 18.": "Widget a généré une erreur 18.",
-                "Widget could not be updated.": "Widget n'a pas pu être mis-à-jour.",
-                "Do you want to update widget?": "Voulez-vous mettre à jour widget?",
-            }
-        };
+        const fr = JSON.parse(buffer);
 
         Engine.#i18next = i18next;
 
