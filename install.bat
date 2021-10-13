38e056281c73b5068f23939718c677e3a65c14f8
8ctopus <hello@octopuslabs.io>
2021-10-13 16:32:05 +0500

    updated: dir architecture

--- install.bat
+++ install.bat
@@ -1,7 +1,13 @@
+mkdir bin\win-x32
+
+cd bin\win-x32
+
 REM sciter 4.4.8.10
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/scapp.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/inspector.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/sciter.dll
+
+cd ..
 
 REM sciter package manager 0.1.6
 curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.6/spm.phar
